01fb1165c18f8e8c133cf8b9d76a495177c696b8
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2021-05-06 18:39:41 -0700

    Enemy Spawner

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -7,13 +7,42 @@ using UnityEditorInternal;
 public class EnemyManager : MonoBehaviour
 {
     public Wave enemyWave;
-    
+
+    IEnumerator Start()
+    {
+        StartCoroutine(SpawnEnemies(enemyWave.cooldown));
+
+        yield break;
+
+    }
+
+    IEnumerator SpawnEnemies(float cooldownBetweenWaves)
+    {
+        for (int i = 0; i < enemyWave.groups.Length; i++)
+        {
+            if (enemyWave.groups[i].enemyTypes.Length > 0)
+            {
+                Group group = enemyWave.groups[i];
+                for (int j = 0; j < group.enemyTypes.Length; j++)
+                {
+                    Enemy newEnemy = Instantiate(group.enemyTypes[j].enemy, group.spawnPoint).GetComponent<Enemy>();
+                    newEnemy.GetComponent<Rigidbody2D>().velocity = new Vector2(group.moveX, group.moveY);
+                    yield return new WaitForSeconds(group.enemyTypes[j].cooldown);
+
+                }
+            }
+
+
+            yield return new WaitForSeconds(cooldownBetweenWaves); // cooldown between groups
+        }
+
+        Debug.Log("done with wave");
+    }
 
     [Serializable]
     public struct Type
     {
         public GameObject enemy;
-        public int count;
         public float cooldown;
     }
 
@@ -21,6 +50,9 @@ public class EnemyManager : MonoBehaviour
     public struct Group
     {
         public Type[] enemyTypes;
+        public Transform spawnPoint;
+        public float moveX;
+        public float moveY;
     }
 
     [Serializable]
